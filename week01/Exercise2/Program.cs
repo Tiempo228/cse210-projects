28468713d1d5762d03a63eef11d74ca4d4504ba7
tiempos <ksanwogou@byupathway.edu>
2025-11-01 23:45:43 +0000

    Exercice 2. conditionals and variables

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage, please: ");
+        int gradePerc = int.Parse(Console.ReadLine());
+        string letter = "";
+        int lastDigit = gradePerc % 10;
+
+        if (gradePerc >= 90)
+        {
+            letter = "A";
+        }
+        else if (80 <= gradePerc && gradePerc < 90)
+        {
+            letter = "B"; ;
+        }
+        else if (70 <= gradePerc && gradePerc < 80)
+        {
+            letter = "C";
+        }
+        else if (60 <= gradePerc && gradePerc < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Letter grade is {letter}");
+
+        if (70 <= gradePerc && gradePerc >= 90)
+        {
+            Console.WriteLine("Congratulation! you passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("S0rry, you failed. Don't give up! Better luck next time!");
+
+        }
+
     }
 }
